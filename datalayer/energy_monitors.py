dee18b31997c18e66271ba7832ce35c69b18ed64
Nikolaos Tasiopoulos <nikolas2000tas@gmail.com>
2022-12-19 12:04:24 +0200

    change class name and add the rest of energy monitor objects

---
+++ datalayer/energy_monitors.py
@@ -0,0 +1,73 @@
+import random
+
+def air_conditioner_energyFactor(temperature):
+  if temperature > 35 or temperature < 12: 
+    return 3
+  elif temperature > 25 or temperature < 16 :
+    return 2
+  else :
+    return 1
+
+def calculate_energyConsumption(temperature, type , randomEnergy, skew):
+    if(randomEnergy):
+        return random.uniform(0, 100 * type)
+    wattsPerHour = air_conditioner_energyFactor(temperature) * type * abs(temperature - 20) * 2 + random.uniform(-0.5+skew, 0.5+skew)
+    if(wattsPerHour > 100 * type):
+        return 100 * type
+    elif(wattsPerHour < 0):
+        return 0
+    elif(abs(temperature - 20) < 3):
+        # return wattsPerHour or 0 with equal probability
+        # print('random choice for wattsPerHour: ', wattsPerHour, ' type: ', type)
+        return random.choice([wattsPerHour, 0, 0])
+    else:
+        return wattsPerHour
+def calculate_energy_base_on_hours(curr_datetime, type , randomEnergy, skew):
+    if(randomEnergy):
+        return random.uniform(0, 100 * type)
+    if(type == 2):
+        # this is a kitchen tool that is using 200Wh 
+        # if curr_datetime.hour >= 13 and curr_datetime.hour <= 15 else np.random.uniform(0, 100 * type)
+        if curr_datetime.hour >= 13 and curr_datetime.hour <= 15:
+            return 100 * type 
+        else:
+            return random.uniform(0, 100 * type)
+    else:
+        # it is a light bulb that is using 100Wh only at night
+        if curr_datetime.hour >= 19 and curr_datetime.hour <= 23:
+            return 100 * type
+        else:
+            return random.uniform(0, 100 * type)
+def calculate_total_energyConsumption(curr_datetime, prev, self):
+    print('hour: ', curr_datetime.hour, ' minute: ', curr_datetime.minute)
+    if(curr_datetime.hour == 0 and curr_datetime.minute == 0):
+
+        w =  2600 * 24 + prev + random.uniform(-1000, 1000)
+        self.prevEtot = w
+        print('returned w: ', w)
+        return w
+
+    else:
+        print('returned prev: ', prev)
+        return prev
+
+class EnergyConsomption():
+    def __init__(self, degresOutside, curr_datetime, skew=0, random=False):
+        self.degreesOutside = degresOutside
+        self.curr_datetime = curr_datetime
+        self.skew = skew
+        self.random = random
+        self.HVAC1 = calculate_energyConsumption(self.degreesOutside, 1 , self.random, self.skew)
+        self.HVAC2 = calculate_energyConsumption(self.degreesOutside, 2 , self.random, self.skew)
+        self.MiAC1 = calculate_energy_base_on_hours(self.curr_datetime, 1.5 , self.random, self.skew)
+        self.MiAC2 = calculate_energy_base_on_hours(self.curr_datetime, 2 , self.random, self.skew)
+        self.prevEtot = 0
+        self.Etot = calculate_total_energyConsumption(self.curr_datetime, self.prevEtot, self)
+    def getConsumption(self, degresOutside, curr_datetime):
+        self.degreesOutside = degresOutside
+        self.curr_datetime = curr_datetime
+        self.HVAC1 = calculate_energyConsumption(self.degreesOutside, 1 , self.random, self.skew)
+        self.HVAC2 = calculate_energyConsumption(self.degreesOutside, 2 , self.random, self.skew)
+        self.MiAC1 = calculate_energy_base_on_hours(self.curr_datetime, 1.5 , self.random, self.skew)
+        self.MiAC2 = calculate_energy_base_on_hours(self.curr_datetime, 2 , self.random, self.skew)
+        self.Etot = calculate_total_energyConsumption(self.curr_datetime, self.prevEtot, self)
--- datalayer/run.py
+++ datalayer/run.py
@@ -2,7 +2,7 @@ from datetime_class import DatetimeClass
 from thermal_sensor import ThermalSensors
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
-from air_condition_energy_monitor import AirConditionEnergyConsomption 
+from energy_monitors import EnergyConsomption 
 
 
 
@@ -15,7 +15,7 @@ def runTest(initDate):
     # TH2 = ThermalSensor(datetime.curr_datetime, skew=-0.5)
 
     # air condition sensors
-    AirCondition = AirConditionEnergyConsomption(Sensors.th1_temp, skew=20, random=False)
+    energyMon = EnergyConsomption(Sensors.th1_temp, curr_datetime=datetime.curr_datetime, skew=20, random=False)
     
 
     hvac1_list = []
@@ -23,6 +23,12 @@ def runTest(initDate):
 
     temp_1_list = []
     temp_2_list = []
+
+    miac1_list = []
+    miac2_list = []
+
+    etot_list = []
+
     date_list = []
     i = 0
     
@@ -34,9 +40,14 @@ def runTest(initDate):
         temp_2 = Sensors.th2_temp
         temp_2_list.append(temp_2)
         # handle air condition
-        hvac1_list.append(AirCondition.HVAC1)
-        hvac2_list.append(AirCondition.HVAC2)
-
+        hvac1_list.append(energyMon.HVAC1)
+        hvac2_list.append(energyMon.HVAC2)
+        # handle miav devices
+        miac1_list.append(energyMon.MiAC1)
+        miac2_list.append(energyMon.MiAC2)
+        # handle total energy
+        
+        etot_list.append(energyMon.Etot)
         # append the date
         date_list.append(datetime.curr_datetime)
 
@@ -44,20 +55,58 @@ def runTest(initDate):
         datetime.step()
         # print(datetime.curr_datetime, '|', temp_1)
         Sensors.step(datetime.curr_datetime)
-        AirCondition.getConsumption(Sensors.th1_temp)
+        energyMon.getConsumption(Sensors.th1_temp, datetime.curr_datetime)
 
         i += 1
         if i == 256:
             break
-    plt.title('Temperature OutSide')
-    plt.plot( date_list, temp_1_list, color='blue')
-    plt.plot( date_list, temp_2_list, color='red')
+    # have 6 subplots
+    # 3 per row
+    # 2 rows
+    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(15, 10))
+    fig.suptitle('Sensor for air condition & temperature')
+    ax1.plot(date_list, temp_1_list, color='blue')
+    ax1.plot(date_list, temp_2_list, color='red')
+    # ax1.set_title('Temperature OutSide')
+    ax1.legend(['TH1', 'TH2'])
+    ax1.set_ylabel('Temperature (C)')
+    ax1.set_xlabel('Date')
+    
+    temp_abs_list = [abs(x-20) for x in temp_1_list]
+    ax2.plot(temp_abs_list, hvac1_list, color='blue')
+    ax2.plot(temp_abs_list, hvac2_list, color='red')
+    ax2.set_title('HVAC vs Temperature')
+    ax2.legend(['HVAC1', 'HVAC2'])
+    ax2.set_ylabel('HVAC (Wh)')
+    ax2.set_xlabel('ABS(Temperature-20) (C)')
+
+    ax3.plot(date_list, hvac1_list, color='blue')
+    ax3.plot(date_list, hvac2_list, color='red')
+    # ax3.set_title('HVAC vs Date')
+    ax3.legend(['HVAC1', 'HVAC2'])
+    ax3.set_ylabel('HVAC (Wh)')
+    ax3.set_xlabel('Date')
+
+    ax4.plot(date_list, miac1_list, color='blue')
+    ax4.plot(date_list, miac2_list, color='red')
+    # ax4.set_title('MiAC vs Date')
+    ax4.legend(['MiAC1', 'MiAC2'])
+    ax4.set_ylabel('MiAC (Wh)')
+    ax4.set_xlabel('Date')
+
+    # ax5 plot as step function
+    ax5.step(date_list, etot_list, color='blue')
+    print(etot_list)
+    print(len(etot_list))
+    # ax5.set_title('Total Energy vs Date')
+    ax5.legend(['Total Energy'])
+    ax5.set_ylabel('Total Energy (Wh)')
+    ax5.set_xlabel('Date')
+
+    # set ax6 null
+    ax6.axis('off')
     plt.show()
 
-    plt.title('HVAC1 and HVAC2')
-    plt.plot( temp_1_list, hvac1_list, color='blue')
-    plt.plot( temp_1_list, hvac2_list, color='red')
-    plt.show()
 # dummyDate = DatetimeClass(wait_secs=0.01)
 # # run test for 2020-01-01 
 # runTest(dummyDate)
