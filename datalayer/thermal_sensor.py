8df217f09d175994c323875b034b252dcf3e537f
Nikolaos Tasiopoulos <nikolas2000tas@gmail.com>
2022-12-19 12:04:32 +0200

    fix temperatures

--- datalayer/thermal_sensor.py
+++ datalayer/thermal_sensor.py
@@ -12,7 +12,7 @@ def get_random_temperature(dt):
     year = dt.year
 
     # Average temperatures in Athens, Greece by month (in Celsius)
-    temperatures = [12.5, 13.5, 16.0, 19.0, 24.0,
+    temperatures = [14, 15, 17, 19.0, 24.0,
                     28.0, 30.5, 30.0, 26.0, 21.0, 16.0, 13.0]
 
     # Get the average temperature for the corresponding month
