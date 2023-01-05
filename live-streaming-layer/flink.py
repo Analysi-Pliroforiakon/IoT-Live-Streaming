b5f395d2dc58b491e498db616533c09564360d47
nikolastas <nikolas2000tas@gmail.com>
2023-01-05 17:34:29 +0200

    some tesating with flink set up

--- live-streaming-layer/flink.py
+++ live-streaming-layer/flink.py
@@ -1,9 +1,16 @@
 import os
 
-FILE = 'C:/Users/Nikolas\Downloads/flink-sql-connector-kafka-1.16.0.jar'
+# import pyflink
 
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment, EnvironmentSettings
+import hbase 
+# from pyflink.common import Row
+# from pyflink.table import (EnvironmentSettings, TableEnvironment, TableDescriptor, Schema,
+#                            DataTypes, FormatDescriptor)
+# from pyflink.table.expressions import lit, col
+# from pyflink.table.udf import udtf
+FILE = '/mnt/c/Users/Nikolas/Downloads/flink-sql-connector-kafka-1.16.0.jar'
 
 
 def main():
@@ -12,9 +19,9 @@ def main():
 
     settings = EnvironmentSettings.new_instance()\
                       .in_streaming_mode()\
-                      .use_blink_planner()\
                       .build()
 
+                    #   .use_blink_planner()\
     # create table environment
     tbl_env = StreamTableEnvironment.create(stream_execution_environment=env,
                                             environment_settings=settings)
@@ -32,11 +39,11 @@ def main():
     #######################################################################
     src_ddl = """
         CREATE TABLE temperatures (
-            date_time TIMESTAMP(3),
-            temp FLOAT,
+            date_time TIMESTAMP,
+            temp DOUBLE
         ) WITH (
             'connector' = 'temperature',
-            'topic' = 'sales-usd',
+            'topic' = 'temperatures',
             'properties.bootstrap.servers' = 'localhost:9092',
             'properties.group.id' = 'foo',
             'format' = 'json'
@@ -46,7 +53,7 @@ def main():
     tbl_env.execute_sql(src_ddl)
 
     # create and initiate loading of source Table
-    tbl = tbl_env.from_path('sales_usd')
+    tbl = tbl_env.from_path('temperatures')
 
     print('\nSource Schema')
     tbl.print_schema()
@@ -64,10 +71,18 @@ def main():
     #       TUMBLE(proctime, INTERVAL '60' SECONDS),
     #       seller_id
     # """
-    # revenue_tbl = tbl_env.sql_query(sql)
+    # connect to a hbase table
+    # sql = """
+    #     SELECT
+    #         date_time,
+    #         temp
+    #     FROM temperatures
+    # """
+    # # create and initiate loading of sink Table
+    # temperatures_tbl = tbl_env.sql_query(sql)
 
     # print('\nProcess Sink Schema')
-    # revenue_tbl.print_schema()
+    # temperatures_tbl.print_schema()
 
     ###############################################################
     # Create Kafka Sink Table
@@ -90,6 +105,26 @@ def main():
     # revenue_tbl.execute_insert('sales_euros').wait()
 
     # tbl_env.execute('windowed-sales-euros')
+
+
+    # ###############################################################
+    # # Create HBase Sink Table
+    # ###############################################################
+    hbase_sink_ddl = """
+        CREATE TABLE htable (
+            rowkey INT,
+            family1 ROW<q1 INT>,
+            family2 ROW<q2 STRING, q3 BIGINT>,
+            family3 ROW<q4 DOUBLE, q5 BOOLEAN, q6 STRING>,
+            PRIMARY KEY (rowkey) NOT ENFORCED 
+        ) WITH (
+            'connector' = 'hbase-2.2',
+            'table-name' = 'test',
+            'zookeeper.quorum' = 'localhost:2181',
+            """
+    hbase_temp = tbl_env.execute_sql(hbase_sink_ddl)
+    print('HBase Sink Table Created')
+    print('hbase_temp: ', hbase_temp.print_schema())
 
 
 if __name__ == '__main__':
