61f8673ceecef5299fab6fbeb142da6f49a709ba
Nikolaos Tasiopoulos <nikolas2000tas@gmail.com>
2023-02-11 18:33:32 +0200

    add agrs test topic and fix bug with Wtot Etot

--- datalayer/run_producer.py
+++ datalayer/run_producer.py
@@ -54,7 +54,9 @@ while True:
     producer.produce('MiAC2', dt.curr_datetime, energyMon.MiAC2)
 
     # handle total energy
-    producer.produce('Etot', dt.curr_datetime, energyMon.Etot)
+    if dt.curr_datetime.hour == 0 and dt.curr_datetime.minute == 0:
+        producer.produce('Etot', dt.curr_datetime, energyMon.Etot)
+        producer.produce('Wtot', dt.curr_datetime, Wtot.get_measurement())
 
     # handle water consumption
     water_1 = W1.get_measurement()
