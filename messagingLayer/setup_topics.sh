d2e4f2256e9d958eee0b1c62a38c2d7ce892f950
Nikolaos Tasiopoulos <nikolas2000tas@gmail.com>
2023-02-17 20:34:47 +0200

    messagingLayer add aggrs topic for aggregations

--- messagingLayer/setup_topics.sh
+++ messagingLayer/setup_topics.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for topic in temperature energy motion water
+for topic in temperature energy motion water aggrs
 do
 	kafka-topics --create --topic $topic --bootstrap-server kafka0:29092 --partitions 2
 done
