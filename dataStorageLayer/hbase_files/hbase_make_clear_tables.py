0b4b088c89512750e3186c20c2d9286961ffdca0
Leonidas Delimpasis <leonidas.delimpasis@twi.gr>
2023-02-28 13:28:18 +0200

    Fixed hbase clear tables script functionality

--- dataStorageLayer/hbase_files/hbase_make_clear_tables.py
+++ dataStorageLayer/hbase_files/hbase_make_clear_tables.py
@@ -3,15 +3,24 @@ connection = happybase.Connection('localhost', 9090)
 tables = ['rawData', 'lateData', 'aggregatedData']
 
 tablesExisted = connection.tables()
+tablesExisted = [table.decode('utf-8') for table in tablesExisted]
 
 for table in tables:
     if table in tablesExisted:
         try:
             connection.delete_table(table, disable=True)
+            print(f'Table {table} deleted.')
+            try:
+                connection.create_table(table, { 'cf': dict() } )
+                print(f'Table {table} created.')
+            except:
+                print(f'Table {table} already exists. Thish should not happen.')
+                pass
+
         except:
-            print(f'Table {table} does not exist')
+            print(f'Table {table} cannot be deleted')
             pass
-    else :
+    else:
         print(f'Table {table} does not exist')
         try:
             connection.create_table(table, { 'cf': dict() } )
